b6574583bfe53ea441e6d26f4ca52df36b420a96
YoutAloK <litematrix161@gmail.com>
2024-07-24 19:50:49 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+let id=0;
+
+let button = document.getElementById("submit");
+
+let UDB={};
+
+button.onclick=()=>{
+    for (const key in UDB) {
+        if (UDB[key].email === email.value) {
+            alert("Invalid email");
+            return;
+        }
+    }
+
+    id++;
+    UDB[id] = {
+        email: email.value,
+        password: password.value
+    };
+}
+    
+    
+    
+    /*id++;
+    UDB[id]={
+        email:email.value,
+        password:password.value
+    }*/
